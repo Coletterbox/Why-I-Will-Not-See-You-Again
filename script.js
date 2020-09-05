3214857db724dd98302b8f1375f90509155ceb66
Colette <colette.rs.shaw@gmail.com>
2020-09-05 17:55:55 +0100

    Add full stop for consistency

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const reasonsArray = [
     "You use the cry laughing emoji in nonsensical places.",
     "You said, \"I'm vegan, if it makes a difference,\" in a context that suggested that you were trying to score points.",
     "You kept asking me questions about \"parents,\" and didn't take the hint when I kept answering them with \"well, my mum...\" and even though I don't feel like you did anything wrong and I hate it when people expect other people to be mindreaders and so I acknowledge that I'm being hypocritical right now, it's just like, come on man.",
-    "I've never seen you speedrun anything",
+    "I've never seen you speedrun anything.",
     "You think that the fact that you're tall matters.",
     "Two words: BATHROOM. CARPET.",
     "I hate your dad.",
