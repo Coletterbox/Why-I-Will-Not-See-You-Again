c9483a99f823af9e99b3b1ac119978306330d562
Colette <colette.rs.shaw@gmail.com>
2020-09-05 17:20:36 +0100

    Add event listeners and test functions

---
+++ script.js
@@ -0,0 +1,17 @@
+function randomArrayItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+const testArray = ["1", "2", "3"];
+
+function displayText() {
+    document.getElementById("reason").textContent = "test";
+}
+
+function runFunctions() {
+    randomArrayItem(testArray);
+    displayText();
+}
+
+window.onload = runFunctions();
+document.getElementById("refresh").addEventListener("click", runFunctions);
