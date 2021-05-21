b79d586c7b3bdf96ab2bedc99a7e19fd7d9129c9
Colette <colette.rs.shaw@gmail.com>
2021-05-21 17:41:26 +0100

    Ugh.

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const reasonsArray = [
     "...you keep doing this infantile thing where the things you say aren't what you mean and everything is some kind of tactic or game.",
     "...when someone is not feeling okay and you automatically jump to pretending you've assumed it's because of something to do with environmental issues (because OF COURSE the only things that could affect YOUR mood are issues outside of yourself and your life, and OBVIOUSLY everyone should be the same way), you're being dismissive of the idea that it's valid for someone to be upset about their own life, which would be fine if it were an accidental side effect of you genuinely being this way, but I'm calling bullshit.",
     "...you're the kind of person who takes pictures with famous people just because they're famous.",
-    "...I haven't seen you in years and I've forgotten how you sound and how you move, and because of your weird emoji use, all the mannerisms I've started to picture you with are stupid as fuck."
+    "...I haven't seen you in years and I've forgotten how you sound and how you move, and because of your weird emoji use, all the mannerisms I've started to picture you with are stupid as fuck.",
+    "...I really fucking hate this new quirk of yours where you use an exclamation mark instead of a question mark EVERY SINGLE TIME. Why? (\"Why!\") I haven't seen you use a question mark in months, and it's super ridiculous especially when it's shit like, \"How are you!\" just every fucking time."
 ];
 
 // alternative colours
