12d8c0cb42ff2227374bb61f89ba4ecfc4f14cf6
Colette <38261091+Coletterbox@users.noreply.github.com>
2021-12-06 23:55:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,8 @@ const reasonsArray = [
     "...I don't have your number anymore.",
     "...other people actually put the effort in and don't have a one-track mind. I'm still rooting for you, but you're going to run out of time.",
     "...if random people on the internet are good enough for you, I don't want you anymore. That would just be a waste.",
-    "...you got distant, so I lost interest. I used to really want to hear from you. Now when I see your name, I'm disappointed."
+    "...you got distant, so I lost interest. I used to really want to hear from you. Now when I see your name, I'm disappointed.",
+    "...I need to at least be able to convincingly role-play thinking you're smarter than me."
 ];
 
 // alternative colours
