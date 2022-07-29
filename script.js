25bd0fee7a978b9630d265ba585cbe08823e1fab
Colette <38261091+Coletterbox@users.noreply.github.com>
2022-07-29 17:54:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,8 @@ const reasonsArray = [
     "...I can see that you\'re just going to continue to be mean as hell and try to bring me down every time you remember that I\'m not exclusively yours, even though you\'ve never attempted to make that happen. You don\'t want to commit to treating me properly and actually being there, but you want me to somehow not have better options.",
     "...you will always get defensive if I have an issue with you, and nothing will ever be fixed.",
     "...I have a rule against fucking anyone I've ever not been in contact with for 60 days, and that was all you seemed to want out of me anyway.",
-    "...it feels weirdly peaceful finally knowing that you really were just using me, after over a year of being confused. I realise now that I did nothing wrong to make you stop being nice to me - you just didn\'t feel like you had a use for me anymore. And I did nothing to make you stop putting effort in - I\'m just too far away to be of use. You wondered why I wasn\'t enjoying it, and now you\'ll never get your answers, but I have mine."
+    "...it feels weirdly peaceful finally knowing that you really were just using me, after over a year of being confused. I realise now that I did nothing wrong to make you stop being nice to me - you just didn\'t feel like you had a use for me anymore. And I did nothing to make you stop putting effort in - I\'m just too far away to be of use. You wondered why I wasn\'t enjoying it, and now you\'ll never get your answers, but I have mine.",
+    "...I suddenly remembered the version of you that made me like you so much, and it feels terrible. That person is long gone, and I need to stop hoping he\'s still in there, somewhere."
 ];
 
 // alternative colours
